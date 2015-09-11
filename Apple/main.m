a009883e1f635ec50e878ec69e357da74287880f
Anton <anton@antons-macbook-pro.local>
2015-09-11 10:04:00 -0700

    Initial Commit

---
+++ Apple/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Apple
+//
+//  Created by Anton on 9/11/15.
+//  Copyright © 2015 Anton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
